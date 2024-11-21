45bcd8209809a3f401d052f42dab241661156d60
Fahim047 <fahimulislam58@gmail.com>
2024-11-21 08:03:13 +0600

    💄 A section added to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from '../components/home/Banner';
+import SupportSection from '../components/home/SupportSection';
 
 const Home = () => {
 	return (
 		<>
 			<Banner />
+			<SupportSection />
 		</>
 	);
 };
